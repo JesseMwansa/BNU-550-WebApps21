2fd6555510d0f3d6f0f870641ce917748a65723c
JesseMwansa <91388688+JesseMwansa@users.noreply.github.com>
2022-10-21 01:08:42 +0100

    razor login and username

--- currencyConverter/Pages/Index.cshtml.cs
+++ currencyConverter/Pages/Index.cshtml.cs
@@ -5,16 +5,31 @@ namespace currencyConverter.Pages
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+
+        public string FullName { get; set; }
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
-
         public void OnGet()
         {
-
+            FullName = "Jesse Mwansa";
+        }
+        public void OnPost()
+        {
+            if(String.IsNullOrWhiteSpace(FullName))
+            {
+                ViewData["NoName"] = "You have not entered a name!";
+                FullName = "Anonymous";
+            }
+            else
+            {
+                ViewData["Message"] = "Name is registered!";
+                // register the user
+            }
         }
     }
 }
